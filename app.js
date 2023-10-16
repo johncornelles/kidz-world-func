86ee00c0476667b90629eb6a7e4ecdeb10664779
johncornelles <john.cornellews@kalvium.community>
2023-10-16 13:58:46 +0530

    negative sir

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-var contents = document.querySelectorAll('.container div div h3');
-var costs = document.querySelectorAll('.buy p');
-var cartitems = document.querySelector('#cart-value');
-var list = [];
-var totalcost = 0;
-var nos = 0;
+const contents = document.querySelectorAll('.container div div h3');
+const costs = document.querySelectorAll('.buy p');
+const cartitems = document.querySelector('#cart-value');
+const list = [];
+let totalcost = 0;
+let nos = 0;
 
 function calc(e) {
-  let name = contents[e].textContent;
-  let costText = costs[e].textContent.replace('$', '');
-  let co = parseFloat(costText);
-  let exist = list.find((item) => item.name === name);
+  const name = contents[e].textContent;
+  const costText = costs[e].textContent.replace('$', '');
+  const co = parseFloat(costText);
+  const exist = list.find((item) => item.name === name);
 
-  if (exist) exist.quantity += 1;
-  else list.push({ name, co, quantity: 1 });
+  if (exist) {
+    exist.quantity += 1;
+  } else {
+    list.push({ name, co, quantity: 1 });
+  }
   totalcost += co;
   nos += 1;
   cartitems.textContent = nos;
@@ -20,12 +23,24 @@ function calc(e) {
 
 const cart = document.querySelector('#cart img');
 cart.onclick = () => {
-  console.clear()
+  console.clear();
   for (let i = 0; i < list.length; i++) {
     console.log(`Item name: ${list[i].name} - Quantity: ${list[i].quantity}`);
   }
 
-  let dollars = Math.floor(totalcost);
-  let cents = Math.round((totalcost - dollars) * 100);
+  const dollars = Math.floor(totalcost);
+  const cents = Math.round((totalcost - dollars) * 100);
+  console.log(`The total amount is ${dollars}$ dollars and ${cents} cents`);
+};
+
+const cartp = document.querySelector('#cart p');
+cartp.onclick = () => {
+  console.clear();
+  for (let i = 0; i < list.length; i++) {
+    console.log(`Item name: ${list[i].name} - Quantity: ${list[i].quantity}`);
+  }
+
+  const dollars = Math.floor(totalcost);
+  const cents = Math.round((totalcost - dollars) * 100);
   console.log(`The total amount is ${dollars}$ dollars and ${cents} cents`);
 };
